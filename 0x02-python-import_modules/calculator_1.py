1d6c5807009f29f86e5fd1a2774a1aa829046b16
Hethii <ezertobi@gmail.com>
2022-12-01 13:06:54 -0800

    task 1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add(a, b):
+    add = a + b
+    return add
+
+
+def sub(a, b):
+    substract = a - b
+    return substract
+
+
+def mul(a, b):
+    multiply = a * b
+    return multiply
+
+
+def div(a, b):
+    divide = a/b
+    return divide
