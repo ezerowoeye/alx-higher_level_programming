2fb5097d2e98c5d8f58239f76229187677cbd227
Ebenezer Owoeye <71957653+Hethii@users.noreply.github.com>
2023-07-26 18:52:59 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-
-const fileName = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(fileName, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
-});
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
